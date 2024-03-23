446f0d1ffb512e7f55628281d2a68450f6f86dc4
Nikita Kirichenko <nikita.nikita.0413@yandex.ru>
2024-03-23 15:11:49 +0300

    Added the NotFound component (Temporary) #6

---
+++ frontend/src/components/NotFound/NotFound.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const NotFound: React.FC = () => {
+    return (
+        <>
+            <center><h1>404 Not Found</h1></center>
+            <hr></hr>
+            <center>React</center>
+        </>
+    );
+};
+
+export default NotFound;
