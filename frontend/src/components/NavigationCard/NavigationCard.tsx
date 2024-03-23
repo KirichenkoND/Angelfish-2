df2e3bbb3b61d8e99c72e6b4cba6a46f351ecadd
Nikita Kirichenko <nikita.nikita.0413@yandex.ru>
2024-03-23 22:09:09 +0300

    Changed the link behavior in Navigation Card.tsx, remove target blank #7

--- frontend/src/components/NavigationCard/NavigationCard.tsx
+++ frontend/src/components/NavigationCard/NavigationCard.tsx
@@ -2,7 +2,7 @@ import './NavigationCard.scss';
 
 const NavigationCard: React.FC<{ imageUrl: string; title: string; link: string }> = ({ imageUrl, title, link }) => {
     return (
-        <a href={link} className="uk-button uk-button-text" target="_blank">
+        <a href={link} className="uk-button uk-button-text">
             <div className="uk-card uk-card-default uk-card-hover uk-card-body">
                 <div className="uk-card-media-top">
                     <img src={imageUrl} alt={title} />
