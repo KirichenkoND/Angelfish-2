97747d87f16088ac0899ca5d6a624ea213328cbb
Nikita Kirichenko <nikita.nikita.0413@yandex.ru>
2024-03-23 15:09:14 +0300

    Added a proxy server to connect to the API

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        "target": "http://api.systempass.efbo.ru",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 })
