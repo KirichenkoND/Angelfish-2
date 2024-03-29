f0ff96730d533af74730bad44fa7fa624f7c6ee2
Gennady <i3104623@yandex.ru>
2024-03-29 23:13:27 +0300

    header fix

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -3,37 +3,6 @@ import HeaderLink from "../HeaderLink/HeaderLink";
 import logo from "../../assets/react.svg";
 import "./Header.scss"
 
-const HeaderLinks = [
-    {
-        text: "Главная",
-        path: "/",
-    },
-    {
-        text: "Редактор",
-        path: "/editor",
-    },
-    {
-        text: "Попуски",
-        path: "/editor",
-    },
-    {
-        text: "Журнал Посещений",
-        path: "/editor",
-    },
-    {
-        text: "Поиск",
-        path: "/editor",
-    },
-    {
-        text: "Пользователи",
-        path: "/editor",
-    },
-    {
-        text: "Настройки",
-        path: "/editor",
-    },
-];
-
 export const Header: React.FC = () => {
     return (
         <>
@@ -41,17 +10,6 @@ export const Header: React.FC = () => {
                 <div className="header">
                     <div className="header-logo">
                         <HeaderLink image={logo} path="/" />
-                    </div>
-                    <div className="header-links">
-                        {HeaderLinks.map((headerLink, i) => {
-                            return (
-                                <HeaderLink
-                                    key={i}
-                                    text={headerLink.text}
-                                    path={headerLink.path}
-                                />
-                            );
-                        })}
                     </div>
                 </div>
             </header>
