ae77f90443fc59560f2e7ff4384f9b2820dabe6a
Nikita Kirichenko <nikita.nikita.0413@yandex.ru>
2024-03-29 22:39:28 +0300

    Create editor page for any tasks

---
+++ frontend/src/pages/AccessEditorPage/AccessEditorPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AccessEditorPage: React.FC = () => {
+    return (
+        <>
+            <h1>Работа с пропусками</h1>
+        </>
+    );
+};
+
+export default AccessEditorPage;
