947313b0a0049557d01b6dca8f0439523c41b505
ItsEthra <107059409+ItsEthra@users.noreply.github.com>
2024-03-29 16:57:54 +0300

    Fetch permissions

--- backend/src/routes/perms.rs
+++ backend/src/routes/perms.rs
@@ -1,7 +1,69 @@
-use super::{Json, RouteResult, RouteState};
+use super::{Json, Query, RouteResult, RouteState};
 use crate::models::{Permission, Target, User};
 use axum::{extract::State, http::Method, routing::*};
-use sqlx::PgPool;
+use serde::{Deserialize, Serialize};
+use sqlx::{prelude::FromRow, PgPool};
+use uuid::Uuid;
+
+#[derive(Deserialize)]
+struct FetchQuery {
+    room_id: Option<i32>,
+    category: Option<String>,
+    role: Option<String>,
+    person_uuid: Option<Uuid>,
+    count: Option<i64>,
+    offset: Option<i64>,
+}
+
+#[derive(Serialize, FromRow)]
+struct Out {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    person_uuid: Option<Uuid>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    category: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    role: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    room_id: Option<i32>,
+}
+
+async fn fetch(
+    State(db): RouteState,
+    Query(query): Query<FetchQuery>,
+) -> RouteResult<Json<Vec<Out>>> {
+    let FetchQuery {
+        room_id,
+        category,
+        role,
+        person_uuid,
+        count,
+        offset,
+    } = query;
+
+    let records = sqlx::query_as::<_, Out>(
+        r#"
+        SELECT * FROM Permission
+        LEFT JOIN Role ON Role.id = role_id
+        LEFT JOIN Category ON Category.id = category_id
+        WHERE
+            coalesce(room_id = $3, true) AND
+            coalesce(category_id = (SELECT id FROM Category WHERE category = $4), true) AND
+            coalesce(role_id = (SELECT id FROM Role WHERE role = $5), true) AND
+            coalesce(person_uuid = $6, true)
+        LIMIT $1 OFFSET $2
+    "#,
+    )
+    .bind(count.unwrap_or(25).max(0))
+    .bind(offset.unwrap_or(0).max(0))
+    .bind(room_id)
+    .bind(category)
+    .bind(role)
+    .bind(person_uuid)
+    .fetch_all(&db)
+    .await?;
+
+    Ok(Json(records))
+}
 
 async fn change_perms(
     method: Method,
@@ -58,5 +120,5 @@ async fn change_perms(
 }
 
 pub fn router() -> Router<PgPool> {
-    Router::new().route("/", post(change_perms).delete(change_perms))
+    Router::new().route("/", get(fetch).post(change_perms).delete(change_perms))
 }
